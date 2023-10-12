8bed679eddadd07819265a3c76f45b0a26cf1444
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-10-13 02:08:37 +0530

    initial

--- script.js
+++ script.js
@@ -415,3 +415,8 @@ console.log(age1,age2)
 const firstWithdrawal = movements.find(mov => mov < 0);
 console.log(movements);
 console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
